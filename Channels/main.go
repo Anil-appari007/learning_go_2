cd6c4772aa5600f68a20ce41a1084e6a0ad621ff
anil-appari007 <anil.appari007@gmail.com>
2024-05-02 22:15:00 +0530

    Added Channels

--- Channels/main.go
+++ Channels/main.go
@@ -2,87 +2,34 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-/*
-
- */
-
-func testFunc(ch chan int, count int) {
-	count++
-	ch <- count
+func waitForAll(allData []int, ch chan int) {
+	total := 0
+	for _, each := range allData {
+		fmt.Println("Sleeping for", each)
+		time.Sleep(time.Duration(each) * time.Second)
+		total++
+	}
+	ch <- total
 }
-
-// func sleep(item string, indexOfItem int, tL int, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	fmt.Println(item, indexOfItem)
-// 	time.Sleep(4 * time.Second)
-// 	fmt.Printf("sleep done for %s. Count:%d/%d \n", item, indexOfItem, tL)
-// }
-
-func sleep(item string, indexOfItem int, tL int, wg *sync.WaitGroup, count *int, m *sync.Mutex) {
-	defer wg.Done()
-	fmt.Println(item, indexOfItem)
-	time.Sleep(4 * time.Second)
-	m.Lock()
-	*count++
-	m.Unlock()
-	// fmt.Printf("sleep done for %s. Count:%d/%d \n", item, indexOfItem, tL)
-	fmt.Printf("sleep done for %s. Count:%d/%d \n", item, *count, tL)
-
-}
-
-// func sleep(item string, indexOfItem int, tL int, wg *sync.WaitGroup, ch chan int) {
-// 	defer wg.Done()
-// 	fmt.Println(item, indexOfItem)
-// 	time.Sleep(4 * time.Second)
-// 	tL--
-// 	ch <- tL
-// 	fmt.Printf("sleep done for %s. Count:%d/%d \n", item, indexOfItem, tL)
-// }
 
 func main() {
+	fmt.Println("Channels")
 	ch := make(chan int)
-	var count int
-	// ch := new(chan int)
-	go testFunc(ch, count)
-	a := <-ch
 
-	go testFunc(ch, a)
-	a = <-ch
+	go func() {
+		time.Sleep(5 * time.Second)
+		ch <- 100
+	}()
+	v := <-ch
+	fmt.Println("v =", v)
 
-	fmt.Println(a)
+	waitTime := []int{1, 2, 3, 4}
+	ch2 := make(chan int)
+	go waitForAll(waitTime, ch2)
 
-	// iCount := 0
-	// cCount := 0
-
-	items := [4]string{"a", "b", "c", "d"}
-	var wg sync.WaitGroup
-	// ch2 := make(chan int)
-	tL := len(items)
-	// for n, each := range items {
-	// 	fmt.Println(each)
-	// 	wg.Add(1)
-	// 	go sleep(each, n, tL, &wg)
-	// }
-
-	c := 0
-	var m sync.Mutex
-
-	for n, each := range items {
-		fmt.Println(each)
-		wg.Add(1)
-		go sleep(each, n, tL, &wg, &c, &m)
-	}
-
-	// for n, each := range items {
-	// 	fmt.Println(each)
-	// 	wg.Add(1)
-	// 	go sleep(each, n, tL, &wg, ch2)
-	// 	tL = <-ch2
-	// }
-	wg.Wait()
-
+	totalValue := <-ch2
+	fmt.Println("Total slept time", totalValue)
 }
