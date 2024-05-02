494951e1465a92f5962d77a6fd39faf100811c49
anil-appari007 <anil.appari007@gmail.com>
2024-05-02 21:54:48 +0530

    Added Generics

--- Generics/main.go
+++ Generics/main.go
@@ -2,21 +2,35 @@ package main
 
 import "fmt"
 
-func addNum[T int | float32 | float64](a []T) T {
-	var Sum T
-	for _, each := range a {
-		Sum += each
-		// fmt.Println(each)
-	}
+func addInt(a, b int) (int, error) {
+	return a + b, nil
+}
 
-	// fmt.Println(Sum)
-	return Sum
+func addFloats(a, b float64) (float64, error) {
+	return a + b, nil
+}
+
+func addNums[T int | float64](a, b T) T {
+	return a + b
 }
 
 func main() {
-	a := []int{1, 2, 3}
-	fmt.Println(addNum(a))
+	fmt.Println("Generics")
+	sum1, err := addInt(1, 2)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("sum1 = %d", sum1)
+	}
 
-	b := []float32{1, 2.3, 3.4}
-	fmt.Println(addNum(b))
+	float1, err := addFloats(1.5, 2.5)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("\nFloat1 = %v", float1)
+	}
+
+	fmt.Println("\nnums1 =", addNums(1, 2))
+	fmt.Println("\nnums1 =", addNums(1.22, 2.43))
+
 }
