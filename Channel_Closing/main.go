2c9568bdc7b0f968309c2f36ea68156c717558fd
anil-appari007 <anil.appari007@gmail.com>
2024-05-05 00:20:21 +0530

    Added Channel_Closing

--- Channel_Closing/main.go
+++ Channel_Closing/main.go
@@ -37,4 +37,23 @@ func main() {
 	// IF data send to close channel
 	// program will panic
 	// tasks <- 333 // panic: send on closed channel
+
+	ch2 := make(chan int, 1)
+	chanCloseStatus := false
+
+	if chanCloseStatus {
+		fmt.Println("Channel is close")
+	} else {
+		fmt.Println("Channel is open")
+		ch2 <- 333
+		close(ch2)
+		fmt.Println(<-ch2)
+		fmt.Println("Channel is closed")
+
+		/*
+			panic: close of closed channel
+		*/
+		// close(ch2)
+
+	}
 }
