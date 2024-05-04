7c4d98f5842eac6c8bbcf8007b8250e81467a6f1
anil-appari007 <anil.appari007@gmail.com>
2024-05-05 00:02:43 +0530

    Added Channel_Closing

---
+++ Channel_Closing/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tasks := make(chan int, 5)
+	taskDone := make(chan bool)
+
+	go func() {
+		for {
+			task, more := <-tasks
+			if more {
+				fmt.Println("Received task", task)
+			} else {
+				fmt.Println("Received All Tasks")
+				taskDone <- true
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 4; i++ {
+		fmt.Println("Adding Task", i)
+		tasks <- i
+		fmt.Println("Added Task", i)
+		time.Sleep(2 * time.Second)
+	}
+	close(tasks)
+	fmt.Println("Tasks are added")
+	<-taskDone
+	_, ok := <-tasks
+	fmt.Println("Received more tasks", ok)
+}
