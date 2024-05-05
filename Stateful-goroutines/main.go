58408257b2d4d34f47b47515864c3efee58e60d0
anil-appari007 <anil.appari007@gmail.com>
2024-05-05 22:49:17 +0530

    Added Stateful-goroutines

--- Stateful-goroutines/main.go
+++ Stateful-goroutines/main.go
@@ -11,7 +11,7 @@ func main() {
 	ch := make(chan int)
 	var wg sync.WaitGroup
 
-	for t := 1; t <= 5; t++ {
+	for t := 1; t <= 15; t++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
@@ -30,3 +30,30 @@ func main() {
 	wg.Wait()
 	fmt.Println("Count:", counter.Load())
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func main() {
+// 	var wg sync.WaitGroup
+// 	var mutex sync.Mutex
+// 	count := 0
+// 	for i := 1; i <= 50; i++ {
+// 		wg.Add(1)
+// 		go func(wg *sync.WaitGroup, mutex *sync.Mutex, count *int, i int) {
+// 			defer wg.Done()
+// 			fmt.Println(i)
+// 			// time.Sleep(1 * time.Second)
+// 			mutex.Lock()
+// 			*count++
+// 			mutex.Unlock()
+// 		}(&wg, &mutex, &count, i)
+// 	}
+// 	wg.Wait()
+// 	total := &count
+// 	fmt.Printf("Count: %d", *total)
+// }
