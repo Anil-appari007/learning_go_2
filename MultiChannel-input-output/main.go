b929c8b647798643ec86ba0a90a33cd00d74c35d
anil-appari007 <anil.appari007@gmail.com>
2024-05-18 13:47:57 +0530

    multiroutines, Multi-channels, input-output, Log

--- MultiChannel-input-output/main.go
+++ MultiChannel-input-output/main.go
@@ -1,28 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"strconv"
 	"time"
 )
 
+// Working
+// func worker(id int, toDo chan string, done chan string) {
+// 	log.Printf("Worker started id: %v\n", id)
+// 	c := 0
+// 	for {
+// 		select {
+// 		case item := <-toDo:
+// 			c++
+// 			log.Printf("Worker %v received %s\n", id, item)
+// 			time.Sleep(2 * time.Second)
+// 			log.Printf("Worker %v completed processing of %s\n", id, item)
+// 			done <- item
+// 		}
+// 		log.Printf("Tasks completed by worker %v = %v\n", id, c)
+
+// 	}
+// }
+
+/*
+output of above
+....
+Worker 1 received
+29/30 Task 30 completed
+Worker 0 completed processing of 29
+Tasks completed by worker 0 = 15
+Worker 0 received
+30/30 Task 29 completed
+
+Issue: function is going through chan even after it is empty
+*/
+
+/////////////////////////////
+
 func worker(id int, toDo chan string, done chan string) {
-	fmt.Printf("Worker started id: %v\n", id)
+	log.Printf("Worker started id: %v\n", id)
 	c := 0
-	for {
-		select {
-		case item := <-toDo:
-			c++
-			fmt.Printf("Worker %v received %s\n", id, item)
-			time.Sleep(2 * time.Second)
-			fmt.Printf("Worker %v completed processing of %s\n", id, item)
-			done <- item
-		}
-		fmt.Printf("Tasks completed by worker %v = %v\n", id, c)
+	for item := range toDo {
+		// select {
+		// case item := <-toDo:
+		c++
+		log.Printf("Worker %v received task %s\n", id, item)
+		time.Sleep(2 * time.Second)
+		log.Printf("Worker %v completed processing of %s\n", id, item)
+		done <- item
+		// }
+		log.Printf("Tasks completed by worker %v = %v\n", id, c)
 
 	}
+	log.Printf("")
 }
 
+/*
+
+Tasks completed by worker 1 = 10
+28/30 Task 29 completed
+Worker 2 completed processing of 28
+Tasks completed by worker 2 = 10
+29/30 Task 28 completed
+Worker 0 completed processing of 30
+Tasks completed by worker 0 = 10
+30/30 Task 30 completed
+*/
+
+// /////////////////////
 func main() {
 
 	/*
@@ -31,12 +78,13 @@ func main() {
 		3. Give input to chan-1
 		4. Send output from Function to Chan-2
 	*/
+	log.Println("Test")
 	l := 30
-	fmt.Printf("Tasks Count: %v\n", l)
+	log.Printf("Tasks Count: %v\n", l)
 	toDo := make(chan string, l)
 	done := make(chan string, l)
 
-	t := 3
+	t := 7
 	for each := range t {
 		go worker(each, toDo, done)
 	}
@@ -50,7 +98,7 @@ func main() {
 		select {
 		case doneTask := <-done:
 			cId++
-			fmt.Printf("%v/%v Task %s completed\n", cId, l, doneTask)
+			log.Printf("%v/%v Task %s completed\n", cId, l, doneTask)
 		}
 	}
 }
